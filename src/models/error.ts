e7d1b3b51f3c109cf1d04325cef56243d24c2f3e
Luiz Valverde <luizcastroval321@gmail.com>
2023-01-18 10:00:30 -0300

    refactor: error classes

--- src/models/error.ts
+++ src/models/error.ts
@@ -1,36 +1,23 @@
-export interface GenError  {
+export interface CustomError  {
     name: string,
     message: string
-    stack?: string
     statusCode: number
 }
 
-export class CustomError implements GenError{
-    public name : string
-    public message: string
-    public stack?: string 
-    public statusCode: number
-
-    public constructor(name:string, statusCode: number, message: string){
-        this.name = name
-        this.statusCode=(statusCode)
-        this.message = message
-    }
-}
-export class NotFoundError extends CustomError {
+export class NotFoundError implements CustomError {
    
     public statusCode = 404
+    public name = "NotFound Error"
+    public message: string
 
     public constructor(subject:string){
-        super("NotFound Error", 404, subject + " not found")
+        this.message = subject + " not found"
     }
 
 }
 
-export class InvalidIdError extends CustomError {
+export class InvalidIdError implements CustomError {
     public statusCode = 400
-    
-    public constructor(){
-        super("InvalidId Error", 400, "Invalid Id")
-    }
+    public message = "Invalid id value"
+    public name =  "InvalidIdError"
 }
